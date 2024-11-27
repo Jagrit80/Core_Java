f3cd510062e68e15b679c4e484e1924faf3c9256
jagrit80 <jagritdewan80@gmail.com>
2024-11-28 00:14:31 +0530

    Removed the old src folder and set up new folder structure: 20_Days_Java_Revision.

---
+++ 20_Days_Java_Revision/Day01/Lectures/GetterSettter/Test/GetterTest.java
@@ -0,0 +1,11 @@
+package GetterSettter.Test;
+
+import gettersetter.Car;
+
+public class GetterTest {
+
+    public static void main(String[] args) {
+        Car car = new Car(100000,5.0,"Maruti","Black");
+        System.out.printf("%s %s",car.getColor(),car.getModel());
+    }
+}
---
+++ 20_Days_Java_Revision/Day01/Practice_Questions/Jagrit/Dewan/Challenge77/Geometry/Circle.java
@@ -0,0 +1,9 @@
+package Jagrit.Dewan.Challenge77.Geometry;
+
+public class Circle {
+public double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
