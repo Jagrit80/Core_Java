4241f97f9df21ddd590d7fe4bdb20650e426d53e
jagrit80 <jagritdewan80@gmail.com>
2024-11-28 00:22:35 +0530

    Removed the old src folder and set up new folder structure: 20_Days_Java_Revision.

---
+++ 20_Days_Java_Revision/Day02/Practice_Questions/Jagrit/Dewan/Challenge77/Geometry/Rectangle.java
@@ -0,0 +1,11 @@
+package Jagrit.Dewan.Challenge77.Geometry;
+
+public class Rectangle {
+public int length;
+public int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+}
