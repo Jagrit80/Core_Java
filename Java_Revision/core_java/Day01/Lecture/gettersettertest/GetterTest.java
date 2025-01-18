93406cb639bd2e76f7d1e6a037a7b759efb04259
Jagrit <jagritdewan80@gmail.com>
2025-01-18 14:44:52 +0530

    Changed

--- Java_Revision/core_java/Day01/Lecture/gettersettertest/GetterTest.java
+++ Java_Revision/core_java/Day01/Lecture/gettersettertest/GetterTest.java
@@ -1,10 +1,10 @@
-package core_java.Day01.Lecture.gettersettertest;
+package Day01.Lecture.gettersettertest;
 
 import gettersetter.Car;
 
 public class GetterTest {
 
-    public static void main(String[] args) {
+    public static <Car> void main(String[] args) {
         Car car = new Car(100000,5.0,"Maruti","Black");
         System.out.printf("%s %s",car.getColor(),car.getModel());
     }
---
+++ Java_Revision/core_java/Day02/Main.java
@@ -0,0 +1,7 @@
+package Day02;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a class");
+    }
+}
